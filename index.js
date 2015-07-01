b5fb89e2d1fb5b2bce765c47a5efbf38b8508800
Mika <mika@mii.moe>
2015-07-01 18:14:40 -0400

    Fixed backward sync problem; now works properly

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const pref = require('sdk/simple-prefs');
 const tabMod = require('resource:///modules/NewTabURL.jsm').NewTabURL;
-
+const { PrefsTarget } = require('sdk/preferences/event-target');
+const home = PrefsTarget({ branchName: "browser.startup." });
 
 const dNewtab = {
 
-	init : function(){
+	init : function() {
 		this.onPrefChange();
 	},
 
-	onPrefChange : function(){
+	onPrefChange : function() {
+		if (pref.prefs.syncHomeNew)
+			pref.prefs.dURL = home.prefs["homepage"];
+
 		if (pref.prefs.dURL != 'about:newtab')
 			tabMod.override(pref.prefs.dURL);
-		if (pref.prefs.syncHomeNew)
-			require('sdk/preferences/service').set("browser.startup.homepage", pref.prefs.dURL);
-		//console.log(tabMod._overridden);
 	},
 
-	onPrefReset: function() {
+	onPrefReset : function() {
 		tabMod.reset();
 		pref.prefs.dURL = 'about:newtab';
-		//console.log(tabMod._overridden);
-	}
+	},
 
+	onHomeSync : function() {
+		home.on("homepage", () => {
+			if (pref.prefs.syncHomeNew)
+				pref.prefs.dURL = home.prefs["homepage"];
+		});
+
+		dNewtab.onPrefChange.call();
+	}
 };
 
-exports.main = function() {
+exports.main = () => {
 	dNewtab.init();
 	pref.on('dURL', dNewtab.onPrefChange);
 	pref.on('dURLreset', dNewtab.onPrefReset);
+	pref.on('syncHomeNew', dNewtab.onHomeSync);
 }
 
-exports.onUnload = function(reason) {
+exports.onUnload = (reason) => {
 	if (reason == "disable" && pref.prefs.resetOnUninstall)
 		dNewtab.onPrefReset();
 }
